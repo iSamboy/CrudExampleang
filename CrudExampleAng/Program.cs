d479b3e57656902598170470099f04caeb76784a
iSamboy <samueldjx@gmail.com>
2023-04-22 00:10:08 -0400

    Creating de API Requests (Office List)

--- CrudExampleAng/Program.cs
+++ CrudExampleAng/Program.cs
@@ -41,28 +41,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+#region API REST REQUESTS 
 
-app.MapGet("/weatherforecast", () =>
+//Office List
+
+app.MapGet("/office/list", async (
+    IOfficeService _officeService,
+    IMapper _mapper
+    ) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var OfficeList = await _officeService.GetList();
+    var OfficeListDTO = _mapper.Map<List<OfficeDTO>>(OfficeList);
+    
+    if(OfficeListDTO.Count > 0)
+        return Results.Ok(OfficeListDTO);
+    else
+        return Results.NotFound();
+
+});
+#endregion
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
