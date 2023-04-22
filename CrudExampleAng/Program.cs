b918e996d395814c9adafc1d99e6bac5507fce69
iSamboy <samueldjx@gmail.com>
2023-04-22 00:58:47 -0400

    Creating the API request (Save, Update and Delete employee)

--- CrudExampleAng/Program.cs
+++ CrudExampleAng/Program.cs
@@ -59,6 +59,90 @@ app.MapGet("/office/list", async (
         return Results.NotFound();
 
 });
+
+// Employee List
+
+app.MapGet("/employee/list", async (
+    IEmployeeService _employeeService,
+    IMapper _mapper
+    ) => {
+        var EmployeeList = await _employeeService.GetList();
+        var EmployeeListDTO = _mapper.Map<List<EmployeeDTO>>(EmployeeList);
+
+        if (EmployeeListDTO.Count > 0)
+            return Results.Ok(EmployeeListDTO);
+        else
+            return Results.NotFound();
+});
+
+// Save entered employees
+
+app.MapPost("/employee/save", async(
+    EmployeeDTO model,
+    IEmployeeService _employeeService,
+    IMapper _mapper
+    ) => { 
+        var _employee = _mapper.Map<Employee>(model);
+        var _createdEmployee = await _employeeService.Add(_employee);
+
+        if(_createdEmployee.IdPerson != 0)
+            return Results.Ok(_mapper.Map<EmployeeDTO>(_createdEmployee));
+        else
+            return Results.StatusCode(StatusCodes.Status500InternalServerError);
+});
+
+
+// Update entered employees
+app.MapPut("/employee/update/{idPerson}", async (
+    int idPerson,
+    EmployeeDTO model,
+    IEmployeeService _employeeService,
+    IMapper _mapper
+    ) => { 
+
+        var _located= await _employeeService.Get(idPerson);
+
+        if (_located is null)
+            return Results.NotFound();
+
+        var _employee = _mapper.Map<Employee>(model);
+
+        _located.FullName = _employee.FullName;
+        _located.IdOffice = _employee.IdOffice;
+        _located.Salary = _employee.Salary;
+        _located.ContractDate = _employee.ContractDate;
+
+        var response = await _employeeService.Update(_located);
+        
+        if (response)
+            return Results.Ok(_mapper.Map<EmployeeDTO>(_located));
+        else
+            return Results.StatusCode(StatusCodes.Status500InternalServerError);
+
+    });
+
+
+// Delete entered employees
+app.MapDelete("/employee/delete{idPerson}", async (
+    int idPerson,
+    IEmployeeService _employeeService
+    ) => {
+
+        var _located = await _employeeService.Get(idPerson);
+
+        if (_located is null)
+            return Results.NotFound();
+
+        var response = await _employeeService.Delete(_located);
+
+        if (response)
+            return Results.Ok();
+        else
+            return Results.StatusCode(StatusCodes.Status500InternalServerError);
+
+    });
+
+
 #endregion
 
 app.Run();
