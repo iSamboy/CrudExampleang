20184c110ab749367c2046456915c2924dcb38d7
iSamboy <samueldjx@gmail.com>
2023-04-23 16:31:09 -0400

    importing Angular Material Modules

--- FrontEndCrud/src/app/app.module.ts
+++ FrontEndCrud/src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// To work with Angular reactive forms 
+import {ReactiveFormsModule} from '@angular/forms';
+
+// To work with Http peticions
+import {HttpClientModule} from '@angular/common/http';
+
+// To work with Angular Material components
+import {MatButtonModule} from '@angular/material/button';       // buttons
+import {MatTableModule} from '@angular/material/table';         // tables
+import {MatPaginatorModule} from '@angular/material/paginator'; // paginator tables
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatTableModule, 
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
