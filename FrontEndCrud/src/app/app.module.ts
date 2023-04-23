30536fe0b8243f2a4a5e6fa007cf83252cd97f8e
iSamboy <samueldjx@gmail.com>
2023-04-23 16:50:43 -0400

    importing angular material moment adaptar

--- FrontEndCrud/src/app/app.module.ts
+++ FrontEndCrud/src/app/app.module.ts
@@ -10,11 +10,26 @@ import {ReactiveFormsModule} from '@angular/forms';
 // To work with Http peticions
 import {HttpClientModule} from '@angular/common/http';
 
-// To work with Angular Material components
-import {MatButtonModule} from '@angular/material/button';       // buttons
+// To work with Angular Material components 
+          // (Tables)
 import {MatTableModule} from '@angular/material/table';         // tables
 import {MatPaginatorModule} from '@angular/material/paginator'; // paginator tables
-
+          // (Forms)
+import {MatButtonModule} from '@angular/material/button';        // buttons
+import {MatFormFieldModule} from '@angular/material/form-field'; // forms
+import {MatInputModule} from '@angular/material/input';          // inputs
+import {MatSelectModule} from '@angular/material/select';        // selects
+import {MatDatepickerModule} from '@angular/material/datepicker';// datepickers
+import {MatNativeDateModule} from '@angular/material/core';      // native dates
+import { MomentDateModule } from '@angular/material-moment-adapter'; // date format
+          // (Alerts)
+import {MatSnackBarModule} from '@angular/material/snack-bar';  // snack bar 
+          // (Icons)
+import {MatIconModule} from '@angular/material/icon';           // Icons
+          // (Modals)
+import {MatDialogModule} from '@angular/material/dialog';       // Modals
+          // (Grids)
+import {MatGridListModule} from '@angular/material/grid-list';  // Grids
 
 @NgModule({
   declarations: [
@@ -27,7 +42,17 @@ import {MatPaginatorModule} from '@angular/material/paginator'; // paginator tab
     HttpClientModule,
     MatButtonModule,
     MatTableModule, 
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule, 
+    MomentDateModule,
+    MatSnackBarModule, 
+    MatIconModule,
+    MatDialogModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
